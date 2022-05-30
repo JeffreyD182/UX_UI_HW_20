4d82801f0dd0ce14d9c278843cdd7bb0fdf7d794
JeffreyD182 <jeffreydeleon@outlook.com>
2022-05-29 23:12:38 -0400

    Repository Test

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// JS test
+
+console.log ("The index.js file is working correctly");
+
+// Back to top
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
